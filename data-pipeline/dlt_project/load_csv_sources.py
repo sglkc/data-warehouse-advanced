2f7f7b0791ac131c7f86afc77e9847a57ffa3414
Seya <31957516+sglkc@users.noreply.github.com>
2025-09-05 11:25:22 +0700

    feat(dlt): load csv sources to master postgre

--- data-pipeline/dlt_project/load_csv_sources.py
+++ data-pipeline/dlt_project/load_csv_sources.py
@@ -4,27 +4,49 @@ import dlt
 from dlt.destinations import postgres
 from dlt.sources.filesystem import filesystem, read_csv
 
-pg_connection = os.environ.get("POSTGRES_CONNECTION_URL")
+pg_connection = os.environ.get("POSTGRES_CONNECTION_URL", "")
 
-def load_csv() -> None:
-    if not pg_connection:
-        print("POSTGRES_CONNECTION_URL not set!")
-        return
+sources = [
+    { "file": "Customer", "table": "customers", "merge_key": "CustomerID" },
+    { "file": "Employee", "table": "employees", "merge_key": "EmployeeID" },
+    { "file": "OrderDetails", "table": "order_details", "merge_key": "OrderDetailsID" },
+    { "file": "Orders", "table": "orders", "merge_key": "OrderID" },
+    { "file": "Product", "table": "products", "merge_key": "ProductID" },
+    { "file": "Region", "table": "regions", "merge_key": "RegionID" },
+    { "file": "Warehouse", "table": "warehouses", "merge_key": "WarehouseID" },
+]
 
+def load_csv(
+    file: str,
+    table: str,
+    merge_key: str,
+) -> None:
     source = filesystem(
         bucket_url="./sources",
-        file_glob="*.csv"
+        file_glob=f"{file}.csv",
     ) | read_csv()
 
+    _ = source.apply_hints(
+            # https://dlthub.com/docs/general-usage/incremental-loading
+            # use "merge" to update existing data and add new data without truncating
+            write_disposition="merge",
+            # merge_key isn't always primary_key, but the data is simple
+            primary_key=merge_key,
+            merge_key=merge_key,
+        )
+
     pipeline = dlt.pipeline(
-        pipeline_name="load_csv_sources",
-        destination=postgres(credentials=pg_connection)
+        destination=postgres(credentials=pg_connection),
+        # schema name in pgsql to insert into
+        dataset_name="master",
+        pipeline_name=f"load_{table}_master",
     )
 
-    info = pipeline.run(source)
+    info = pipeline.run(source.with_name(table))
 
-    print(info)
-
+    print(f"{file} load info:", info)
+    print()
 
 if __name__ == "__main__":
-    load_csv()
+    for source in sources:
+        load_csv(source["file"], source["table"], source["merge_key"])
